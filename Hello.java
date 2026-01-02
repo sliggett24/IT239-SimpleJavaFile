cdf198ef2e7c6405af5335c2b79b8ed802aac520
sliggett24 <sliggett24@southhills.edu>
2026-01-02 17:27:40 -0500

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
-public class Hello
+public class Hello 
 {
     public static void main(String[] args) 
     {
-        System.out.println("Hello, world! From: Java :]");
+        System.out.println("Hello there world! From: Java :]");
     }
 }
